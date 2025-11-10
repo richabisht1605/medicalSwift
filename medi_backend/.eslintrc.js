4fe320cdac70e3c985010bce936d83c71c9ca1f2
richabisht1605 <richabisht1605@gmail.com>
2025-11-10 14:24:00 +0530

    feat: backend setup and connection

---
+++ medi_backend/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  env: {
+    node: true,
+    es2021: true,
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/no-explicit-any": "off"
+  },
+};
