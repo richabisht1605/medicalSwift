3f3b377c211e239f0abc001625ea69ceaea9791f
richabisht1605 <richabisht1605@gmail.com>
2025-11-10 14:42:38 +0530

    feat: user model added

---
+++ medi_backend/src/models/userModel.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+export interface IUser extends Document {
+  fullName: string;
+  email: string;
+  password: string;
+  phone: string;
+  role: "user" | "rider";
+  ambulanceName?: string;
+  ambulanceNumber?: string;
+  hasMedicalTraining?: boolean;
+  imageUrl?: string;
+}
+
+const userSchema: Schema = new Schema<IUser>(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: true },
+    role: { type: String, enum: ["user", "rider"], required: true },
+    ambulanceName: { type: String },
+    ambulanceNumber: { type: String },
+    hasMedicalTraining: { type: Boolean },
+    imageUrl: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<IUser>("User", userSchema);
