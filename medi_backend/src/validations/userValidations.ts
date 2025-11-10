de549d2583125a46b8e5826534565931ab1910b8
richabisht1605 <richabisht1605@gmail.com>
2025-11-10 14:47:19 +0530

    feat: userValidations added using joi

---
+++ medi_backend/src/validations/userValidations.ts
@@ -0,0 +1,13 @@
+import Joi from "joi";
+
+export const registerSchema = Joi.object({
+  fullName: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+  password: Joi.string().min(6).required(),
+  role: Joi.string().valid("user", "rider").required(),
+  ambulanceName: Joi.string().when("role", { is: "rider", then: Joi.required() }),
+  ambulanceNumber: Joi.string().when("role", { is: "rider", then: Joi.required() }),
+  hasMedicalTraining: Joi.boolean(),
+  imageUrl: Joi.string().uri().optional(),
+})
